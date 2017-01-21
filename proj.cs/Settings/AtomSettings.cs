437a43d32c7cd3b9bc75ccddeee835ebc0ced5a2
Byron Mayne <ByronMayne@gmail.com>
2017-01-21 14:37:10 -0500

    Updated the AtomSettings template to be much more clear.
    Before it was hard to follow so I refactored it to be much cleaner.

--- proj.cs/Settings/AtomSettings.cs
+++ proj.cs/Settings/AtomSettings.cs
@@ -7,24 +7,37 @@ namespace AtomPackageManager
     [System.Serializable]
     public class AtomSettings
     {
-		[SerializeField]
+        [Header("Project Settings")]
+        [SerializeField]
         private string m_UnityPath = "/Assets/Atom";
+        [Header("Local Values")]
+        private bool m_runInBackground = true;
 
         /// <summary>
         /// The path that Atom should put all the dlls it compiles in your Unity project.
-        /// This value is saved to the project settings of Atom.
+        //  This value is saved to the project settings of Atom.
         /// </summary>
         public string UnityPath
         {
             get { return m_UnityPath; }
-            set { UnityPath = value; }
+            set { m_UnityPath = value; }
         }
-
+        /// <summary>
+        /// Should Atom run in the background and check for updates?
+        // This value is only saved on your local machine.
+        /// </summary>
+        public bool runInBackground
+        {
+            get { return m_runInBackground; }
+            set { m_runInBackground = value; }
+        }
         /// <summary>
         /// Loads all the settings from disk.
         /// </summary>
         public void Load()
         {
+            UnityPath = EditorPrefs.GetString(key:PlayerSettings.productName + ":UnityPath:string", defaultValue:@"/Assets/Atom");
+            runInBackground = EditorPrefs.GetBool(key:PlayerSettings.productName + ":runInBackground:bool", defaultValue:true);
         }
 
         /// <summary>
@@ -32,7 +45,9 @@ namespace AtomPackageManager
         /// </summary>
         public void Save()
         {
-		}
+            EditorPrefs.SetString(key:PlayerSettings.productName + ":UnityPath:string", value:@"/Assets/Atom");
+            EditorPrefs.SetBool(key:PlayerSettings.productName + ":runInBackground:bool", value:true);
+        }
     }
 }
 
